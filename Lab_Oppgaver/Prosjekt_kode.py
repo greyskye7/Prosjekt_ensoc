9003864c48db076073329d86b1a47098225b4f1d
Andreas <andreasroedvikoren@gmail.com>
2019-10-31 20:58:22 +0100

    31.10.2019

--- Lab_Oppgaver/Prosjekt_kode.py
+++ Lab_Oppgaver/Prosjekt_kode.py
@@ -38,33 +38,40 @@ SerialIOmbed = serial.Serial(port,9600) #setup the serial port and baudrate
 SerialIOmbed.flushInput()                #Remove old input's
 SerialIOmbed.flushOutput()
 
-
+a = 0
 
 def loop1():
+    global a
     while True:
         data, addr = sock.recvfrom(1280) # Max recieve size is 1280 bytes
-        print "Verdi:", data
-       
-        
-        if(int(data) == 1):
-            SerialIOmbed.write("1\n")
-        elif(int(data) == 2):
-            SerialIOmbed.write("2\n")
-        elif(int(data) == 3):
-            SerialIOmbed.write("3\n")
-        elif(int(data) == 4):
-            SerialIOmbed.write("4\n")
-        elif(data == 5):
-            SerialIOmbed.write("5\n")
-        else:
-            SerialIOmbed.write("0\n")
+        if(int(data) == 00001):
+            a = 1
+        if(int(data) == 00000):
+            a = 0
+        if(a == 1):
+            print "Verdi:", data
+           
+            
+            if(int(data) == 10001):
+                SerialIOmbed.write("1\n")
+            elif(int(data) == 1001):
+                SerialIOmbed.write("2\n")
+            elif(int(data) == 101):
+                SerialIOmbed.write("3\n")
+            elif(int(data) == 11):
+                SerialIOmbed.write("4\n")
+            elif(data == 5):
+                SerialIOmbed.write("5\n")
+            else:
+                SerialIOmbed.write("0\n")
             
         
 
 def loop2():
+    global a
     while True:     
         bus.write_byte(address, 0x00)
-        time.sleep(0.2)
+        time.sleep(0.1)
         data0 = bus.read_byte(address)
         data1 = bus.read_byte(address)
         data2 = bus.read_byte(address)
@@ -74,64 +81,58 @@ def loop2():
         data = [data0, data1, data2, data3, data4, data5]
         joy_x = data[0]
         joy_y = data[1]
+        Z_button = (data[5] & 0x01)
+        
+        if(a == 0):
               
-        if(joy_x < 134):
-            SerialIOmbed.write("1\n")
-        elif(joy_x > 144):
-            SerialIOmbed.write("2\n")
-        elif(joy_y < 134):
-            SerialIOmbed.write("3\n")
-        elif(joy_y > 144):
-            SerialIOmbed.write("4\n")
-        else:
-            SerialIOmbed.write("0\n")
+            if(joy_x < 134):
+                SerialIOmbed.write("1\n")
+            elif(joy_x > 144):
+                SerialIOmbed.write("2\n")
+            elif(joy_y < 134):
+                SerialIOmbed.write("3\n")
+            elif(joy_y > 144):
+                SerialIOmbed.write("4\n")
+            else:
+                SerialIOmbed.write("0\n")
 
 
 
-def loop3():
-    while True:
-        @app.route('/')
-        def index():
-            """Video streaming home page"""
-            return render_template('index.html')
-
-        def gen():
-            """Video streaming generator function"""
-            while True:
-                rval, frame = vc.read()
-                cv2.imwrite('pic.jpg', frame)
-                yield(b'--frame\r\n'
-                        b'Content-Type: image/jpeg\r\n\r\n' + open('pic.jpg', 'rb').read() + b'\r\n')
-
-        @app.route('/video_feed')
-        def video_feed():
-            return Response(gen(),
-                            mimetype='multipart/x-mixed-replace; boundary=frame')
-
-        if __name__ == '__main__':
-            serve(app, host='10.0.0.79', port=5000)
-            #app.run(host='10.0.0.79', port=5000,  debug=True, threaded=True)
-
+#def loop3():
+    
 thread1 = threading.Thread(target=loop1)
 thread1.start()
 thread2 = threading.Thread(target=loop2)
 thread2.start()
-thread3 = threading.Thread(target=loop3)
-thread3.start()
+#thread3 = threading.Thread(target=loop3)
+#thread3.start()
+
+@app.route('/')
+def index():
+    """Video streaming home page"""
+    return render_template('index.html')
+
+def gen():
+    """Video streaming generator function"""
+    while True:
+        rval, frame = vc.read()
+        cv2.imwrite('pic.jpg', frame)
+        yield(b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + open('pic.jpg', 'rb').read() + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    serve(app, host='10.0.0.79', port=5000)
+    #app.run(host='10.0.0.79', port=5000,  debug=True, threaded=True)
 
 
-while True:
-    rval, frame = vc.read()
-    if rval:
-        cv2.imshow("pic.jpg", frame)
-    else:
-        print("Not working")
-        break
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('q'):
-        break
-vc.release()
-cv2.destroyAllWindows()
+
+
+
 
 
 
